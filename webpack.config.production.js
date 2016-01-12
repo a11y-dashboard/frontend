30a57fafa1fe4b62fe95377dc911a2f77afc72ce
Joscha Feth <jfeth@atlassian.com>
2016-01-13 10:35:51 +1100

    performance: Minify JS

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,8 +1,10 @@
 const objectAssign = require('object-assign');
 const baseConfig = require('./webpack.config.js');
+const webpack = require('webpack');
 
 const config = objectAssign({}, baseConfig, {
 
 });
+config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 
 module.exports = config;
